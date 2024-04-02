54c933acd578eba13869ac3b0b72c1b52f78c53c
KKatiaa <katyakravchuk123@gmail.com>
2024-04-02 16:10:14 +0300

    added new pipeline

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-ENV PORT=80
+ENV PORT=5000
 EXPOSE $PORT
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World! Hello world! Added changes. Added changes 2. Added changes 3. Updated target dir Fixed mistekes. Another changes!!! CHANGES,,, Changes2,,,,Changes3,,,Changes444, dskdhb---fddsgvdf LAST COMMIT');
+    res.end('Hello World!');
 }).listen(PORT);
